2e1576cd5ae0784650b8ded1b5ea89c05f7d4f41
‘1084691005@qq.com’ <1084691005@qq.com>
2023-02-15 20:11:58 +0800

    Revert "1"
    This reverts commit a5c010eb

--- src/main/java/com/zx/medium/pow50/Solution.java
+++ src/main/java/com/zx/medium/pow50/Solution.java
@@ -3,21 +3,17 @@ package com.zx.medium.pow50;
 /**
  * 实现 pow(x, n) ，即计算 x 的整数 n 次幂函数（即，xn ）。
  * 输入：x = 2.00000, n = 10
- * 输出：1024.00000
+ * 输出：1024.00000  ....
  */
-public class Solution {
-    public static double myPow(double x, int n) {
+class Solution {
+    public double myPow(double x, int n) {
         double res = 1.0;
-        for (int i = Math.abs(n); i != 0; i /= 2) {
-            if (i % 2 != 0) {
+        for(int i = Math.abs(n); i != 0; i /= 2){
+            if(i % 2 != 0){
                 res *= x;
             }
             x *= x;
         }
-        return n < 0 ? 1 / res : res;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(myPow(2, 10));
+        return  n < 0 ? 1 / res : res;
     }
 } 
