3deaa4e78d1c6eccba3bb41a99e4ce0a2f89ae05
zhaoxiang <1084691005@qq.com>
2023-02-23 22:34:57 +0800

    83

---
+++ src/main/java/com/zx/easy/删除排序链表中的重复元素83/Solution.java
@@ -0,0 +1,49 @@
+package com.zx.easy.删除排序链表中的重复元素83;
+
+/**
+ * 给定一个已排序的链表的头 head ， 删除所有重复的元素，使每个元素只出现一次 。返回 已排序的链表 。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：head = [1,1,2]
+ * 输出：[1,2]
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：head = [1,1,2,3,3]
+ * 输出：[1,2,3]
+ */
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode curr = head;
+        if (head == null) return null;
+        while (curr.next != null) {
+            if (curr.next.val == curr.val) {
+                curr.next = curr.next.next;
+            } else {
+                curr = curr.next;
+            }
+        }
+        return head;
+    }
+}
